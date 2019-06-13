020d8c546b787b74fb2671a695d70d5e2a3f9afa
ttom921 <ttom921@hotmail.com>
2019-06-13 10:13:49 +0800

    加入rxjd

--- day08rxjs.js
+++ day08rxjs.js
@@ -1,3 +1,5 @@
+import { fromEvent, of } from "rxjs";
+
 // import { interval } from "rxjs";
 // import { map, mapTo, take } from 'rxjs/operators';
 // var source$ = interval(1000);
@@ -18,15 +20,43 @@
 //     error: (err) => { console.log("Error: " + err); },
 //     complete: () => { console.log("complete"); }
 // });
-({
-    plugins: ['jsdom-quokka-plugin'],
-    jsdom: { html: `<div id="test">Hello</div>` }
-})
-import { interval, fromEvent } from "rxjs";
-import { first, takeUntil } from "rxjs/operators";
-var source$ = interval(1000);
-var click = fromEvent(document.body, "click");
-var example$ = source$.pipe(takeUntil(click));
+
+// ({
+//     plugins: ['jsdom-quokka-plugin'],
+//     jsdom: { html: `<div id="test">Hello</div>` }
+// })
+// import { interval, fromEvent } from "rxjs";
+// import { first, takeUntil } from "rxjs/operators";
+// var source$ = interval(1000);
+// var click = fromEvent(document.body, "click");
+// var example$ = source$.pipe(takeUntil(click));
+// example$.subscribe({
+//     next: (value) => { console.log(value); },
+//     error: (err) => { console.log('Error: ' + err); },
+//     complete: () => { console.log('complete'); }
+// });
+
+// import { of, fromEvent } from "rxjs";
+// import { map,concatAll } from 'rxjs/operators';
+// var click = fromEvent(document.body, 'click');
+// var source$ = click.pipe(
+//     map(e => of(1, 2, 3))
+// );
+// var example$ = source$.pipe(concatAll());
+// example$.subscribe({
+//     next: (value) => { console.log(value); },
+//     error: (err) => { console.log('Error: ' + err); },
+//     complete: () => { console.log('complete'); }
+// });
+
+import { interval } from "rxjs";
+import { take, concatAll } from "rxjs/operators";
+var obs1$ = interval(1000).pipe(take(5));
+var obs2$ = interval(500).pipe(take(2));
+var obs3$ = interval(2000).pipe(take(1));
+
+var source$ = of(obs1$, obs2$, obs3$);
+var example$ = source$.pipe(concatAll());
 example$.subscribe({
     next: (value) => { console.log(value); },
     error: (err) => { console.log('Error: ' + err); },
