e5892ef2aeb03fbd3da5d009e6d427a0fe153f9b
ttom921 <ttom921@hotmail.com>
2019-06-26 11:58:13 +0800

    加入day10rxjs.js

--- day10rxjs.js
+++ day10rxjs.js
@@ -1,10 +1,35 @@
-import { interval } from "rxjs";
-import { take, combineLatest } from "rxjs/operators";
+// import { interval } from "rxjs";
+// import { take, combineLatest } from "rxjs/operators";
 
-var source$ = interval(500).pipe(take(3));
-var newest$ = interval(300).pipe(take(6));
+// var source$ = interval(500).pipe(take(3));
+// var newest$ = interval(300).pipe(take(6));
 
-var example$ = source$.pipe(combineLatest(newest$, (x, y) => x + y));
+// var example$ = source$.pipe(combineLatest(newest$, (x, y) => x + y));
+// example$.subscribe({
+//     next: (value) => { console.log(value); },
+//     error: (err) => { console.log('error: ' + err) },
+//     complete: () => { console.log('complete'); }
+// });
+
+// import { interval } from "rxjs";
+// import { take,zip} from "rxjs/operators";
+
+// var source$ = interval(500).pipe(take(3));
+// var newest$ = interval(300).pipe(take(6));
+
+
+// var example$ = source$.pipe(zip(newest$, (x, y) => x + y));
+// example$.subscribe({
+//     next: (value) => { console.log(value); },
+//     error: (err) => { console.log('error: ' + err) },
+//     complete: () => { console.log('complete'); }
+// });
+
+import { from, interval } from "rxjs";
+import { zip } from "rxjs/operators";
+var source$ = from('hello');
+var source2$ = interval(100);
+var example$ = source$.pipe(zip(source2$, (x, y) => x));
 example$.subscribe({
     next: (value) => { console.log(value); },
     error: (err) => { console.log('error: ' + err) },
