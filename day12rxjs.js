55801cfe0ab68eaba379cfb2ef84e234bdeec324
ttom921 <ttom921@hotmail.com>
2019-07-09 10:28:58 +0800

    加入rxjs

--- day12rxjs.js
+++ day12rxjs.js
@@ -5,18 +5,61 @@
 // }, 0);
 // console.log(result);
 
-import { from, interval } from "rxjs";
-import { zip, scan } from "rxjs/operators";
+// import { from, interval } from "rxjs";
+// import { zip, scan } from "rxjs/operators";
 
-var source$ = from("hello").pipe(
-    zip(interval(600), (x, y) => x)
-);
+// var source$ = from("hello").pipe(
+//     zip(interval(600), (x, y) => x)
+// );
+// var example$ = source$.pipe(
+//     scan((origin, next) => origin + next, '')
+// );
+
+// example$.subscribe({
+//     next: (value) => { console.log(value); },
+//     error: (err) => { console.log('Error: ' + err); },
+//     complete: () => { console.log("complete"); }
+// });
+
+// import { interval } from "rxjs";
+// import { buffer } from "rxjs/operators";
+
+// var source$ = interval(300);
+// var source2$ = interval(1000);
+// var example$ = source$.pipe(
+//     buffer(source2$)
+// );
+// example$.subscribe({
+//     next: (value) => { console.log(value); },
+//     error: (err) => { console.log('Error: ' + err); },
+//     complete: () => { console.log('complete'); }
+// });
+
+
+// import { interval } from "rxjs";
+// import { buffer, bufferTime } from "rxjs/operators";
+
+// var source$ = interval(300);
+// var example$ = source$.pipe(
+//     bufferTime(1000)
+// );
+
+// example$.subscribe({
+//     next: (value) => { console.log(value); },
+//     error: (err) => { console.log('Error: ' + err); },
+//     complete: () => { console.log('complete'); }
+// });
+
+
+import { interval } from "rxjs";
+import { bufferCount } from "rxjs/operators";
+
+var source$ = interval(300);
 var example$ = source$.pipe(
-    scan((origin, next) => origin + next, '')
+    bufferCount(3)
 );
-
 example$.subscribe({
     next: (value) => { console.log(value); },
     error: (err) => { console.log('Error: ' + err); },
-    complete: () => { console.log("complete"); }
+    complete: () => { console.log('complete'); }
 });
---
+++ day29.js
@@ -0,0 +1,3 @@
+var list = [1, 2, 3, 4];
+list.length = 0;
+console.log(list);
